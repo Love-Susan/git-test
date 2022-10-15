337147f4c86391ef0d263178f7b5456c522f2947
Susan <1518371393@qq.com>
2022-10-15 21:00:06 +0800

    push commit

--- src/TestBuffered/TestBuffered.java
+++ src/TestBuffered/TestBuffered.java
@@ -6,13 +6,7 @@ public class TestBuffered {
     public static void main(String[] args) throws Exception{
         String str = "zhangsan";
         System.out.println("Hello git");
-        System.out.println("Hello gitHub");
-        System.out.println("Hello gitHub1111");
-        System.out.println("Hello gitHub44444");
-        System.out.println("master test1");
-        System.out.println("hot-fix commit2");
-        System.out.println("gitee test");
-        System.out.println("gitee test2");
+        System.out.println("push test!");
 
         System.out.println("str=" + str);
 
